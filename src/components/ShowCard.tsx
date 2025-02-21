d92c8096fa337e4f16f6aaf0054ec9fbd2212848
TauraNam <tauranamirskaite@gmail.com>
2025-02-21 21:11:07 +0200

    added ShowOverview component

--- src/components/ShowCard.tsx
+++ src/components/ShowCard.tsx
@@ -8,34 +8,36 @@ interface ShowCardProps {
 
 const ShowCard: React.FC<ShowCardProps> = ({ show }) => {
 
-    const stripHtml = (html: string) => { 
+    const stripHtml = (html: string) => {
         const doc = new DOMParser().parseFromString(html, "text/html")
         return doc.body.textContent || ""
     }
 
     return (
         <div className='show-card'>
-            <img src={show.image.medium} alt={show.name} />
-            <div className='card-details'>
-                <div className='card-header'>
-                    <p>{show.name}</p>
-                    <MdOutlineFavoriteBorder />
-                </div>
-                <p className='summary'>
-                    {stripHtml(show.summary)
-                        .split(" ")
-                        .slice(0, 15)
-                        .join(" ") + "..." || ""
-                    }
-                </p>
-                <div className='card-footer'>
-                    <p className='rating'>
-                        <span>Rating</span>
-                        <span>{show.rating.average} / 10 </span>
+            <a href={`/${show.id}`}>
+                <img src={show.image.medium} alt={show.name} />
+                <div className='card-details'>
+                    <div className='card-header'>
+                        <p>{show.name}</p>
+                        <MdOutlineFavoriteBorder />
+                    </div>
+                    <p className='summary'>
+                        {stripHtml(show.summary)
+                            .split(" ")
+                            .slice(0, 15)
+                            .join(" ") + "..." || ""
+                        }
                     </p>
-                    <p className='genres'>{show.genres.join(', ')}</p>
+                    <div className='card-footer'>
+                        <p className='rating'>
+                            <span>Rating</span>
+                            <span>{show.rating.average} / 10 </span>
+                        </p>
+                        <p className='genres'>{show.genres.join(', ')}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     );
 }
---
+++ src/pages/ShowOverview.tsx
@@ -0,0 +1,70 @@
+import { useParams } from "react-router-dom"
+import { Show, ShowsContextType } from '../types/global'
+import { useEffect, useState } from "react"
+import { format } from 'date-fns'
+import { useShowsContext } from "../context/ShowsContext"
+
+const ShowOverview = () => {
+    const { shows } = useShowsContext() as ShowsContextType
+    
+    const { id } = useParams()
+    const [show, setShow] = useState<Show>()
+
+    const getShow = async (): Promise<void> => {
+        if (!id) return
+
+        for (const item of shows) {
+            if (item.id == parseInt(id)) {
+                setShow(item)
+            }
+        }
+    }
+
+    useEffect(() => {
+        getShow()
+    }, [shows])
+
+
+    const stripHtml = (html: string) => {
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent || ""
+    }
+
+    const formattedDate = (date: string) => {
+        return date 
+        ? format(new Date(date), 'EEE MMM dd yyyy')
+        : 'No date available'
+    }
+
+    return (
+        <div>
+            { show && 
+            <div className="show-container"> 
+                <img src={show.image.original} alt={show.name} />
+
+                <div className="show-details">
+                    <div>
+                        <p className="show-name">{show.name}</p>
+                        <p className="add-favorite">Add to favorites</p>
+                        <p> {stripHtml(show.summary)}</p>
+                    </div>
+
+                    <div className="show-info">
+                        <p>Premiered: {formattedDate(show.premiered)}</p>
+                        <p>Ended: {formattedDate(show.ended)}</p>
+                        <p>Average runtime {show.averageRuntime} minutes</p>
+                        <p>Show status: {show.status}</p>
+                        <p>Language: {show.language}</p>
+                        <p>Average rating: {show.rating.average}</p>
+                        <p> Official site:&nbsp;
+                        <a href={show.officialSite}target="_blank" rel="noopener noreferrer" className="link">Go to official site</a>
+                        </p>
+                        <p>Genres: {show.genres.join(', ')}</p>
+                    </div>
+                </div> 
+            </div> }
+        </div>
+    );
+}
+
+export default ShowOverview;
