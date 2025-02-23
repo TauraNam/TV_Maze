a15e7ebcb11cc5568a7e81e655af42d1b446a099
TauraNam <tauranamirskaite@gmail.com>
2025-02-23 17:51:39 +0200

    CSS changes

--- src/pages/ShowOverview.tsx
+++ src/pages/ShowOverview.tsx
@@ -7,7 +7,7 @@ import FavoriteShow from "../components/FavoriteShow"
 
 const ShowOverview = () => {
     const { shows } = useShowsContext() as ShowsContextType
-    
+
     const { id } = useParams()
     const [show, setShow] = useState<Show>()
 
@@ -32,23 +32,22 @@ const ShowOverview = () => {
     }
 
     const formattedDate = (date: string) => {
-        return date 
-        ? format(new Date(date), 'EEE MMM dd yyyy')
-        : 'No date available'
+        return date
+            ? format(new Date(date), 'EEE MMM dd yyyy')
+            : 'No date available'
     }
 
     return (
-        <div>
-            { show && 
-            <div className="show-container"> 
-                <img src={show.image.original} alt={show.name} />
+        <div className="show-overview">
+            {show &&
+                <div className="show-container">
+                    <img src={show.image.original} alt={show.name} />
 
-                <div className="show-details">
-                    <div>
+                    <div className="show-details">
                         <p className="show-name">{show.name}</p>
-                        <FavoriteShow showId={show.id} 
-                        buttonStyle={<p className="add-favorite">Add to favorites</p>} 
-                        buttonStyleActive={<p className="add-favorite is-favorite">Favorite</p>}/>
+                        <FavoriteShow showId={show.id}
+                            buttonStyle={<p className="add-favorite">Add to favorites</p>}
+                            buttonStyleActive={<p className="add-favorite is-favorite">Favorite</p>} />
                         <p> {stripHtml(show.summary)}</p>
                     </div>
 
@@ -60,12 +59,11 @@ const ShowOverview = () => {
                         <p>Language: {show.language}</p>
                         <p>Average rating: {show.rating.average}</p>
                         <p> Official site:&nbsp;
-                        <a href={show.officialSite}target="_blank" rel="noopener noreferrer" className="link">Go to official site</a>
+                            <a href={show.officialSite} target="_blank" rel="noopener noreferrer" className="link">Go to official site</a>
                         </p>
                         <p>Genres: {show.genres.join(', ')}</p>
                     </div>
-                </div> 
-            </div> }
+                </div>}
         </div>
     );
 }
